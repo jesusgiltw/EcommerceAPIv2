bc65f4a6df51e50071a3a41ee1aa3e6e67cdff15
jesus.gil <jesus.gil@thoughtworks.com>
2025-07-09 13:51:26 +0200

    Fixing namespace

---
+++ TestEcommerce/SellersServiceTest.cs
@@ -0,0 +1,16 @@
+using EcommerceAPI.Entities;
+using EcommerceAPI.Interfaces;
+using EcommerceAPI.Services;
+using Moq;
+using Xunit;
+
+namespace TestEcommerce;
+
+public class SellersServiceTest
+{
+    [Fact]
+    public void TestMethod1()
+    {
+        
+    }
+}
